a46697783d018032ee56060c6530bb9b81289dba
Murilo Sartori <murilo.sartori.dev@gmail.com>
2026-03-17 13:35:25 -0300

    ex2

---
+++ ex2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="somadosnumeros.php" method="post">
+       <input type="text" name="num1" id="">
+       <input type="text" name="num2" id=""> 
+       <input type="submit" value="enviar">
+       <input type="reset" value="limpar">
+       
+    </form>
+</body>
+</html>
